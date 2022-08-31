474e6307a4040898f7058bf4b8224f5bb32b03d3
Vivek <vivekyadav2815431@gmail.com>
2022-08-31 21:14:09 +0530

    Admin Router Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminpageComponent } from './adminpage/adminpage.component';
 
 const routes: Routes = [
+  {path: 'admin', component: AdminpageComponent}
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,26 @@ import { NanbarComponent } from './nanbar/nanbar.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { AdminpageComponent } from './adminpage/adminpage.component';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NanbarComponent
+    NanbarComponent,
+    AdminpageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,MatToolbarModule,MatButtonModule,MatIconModule
+    BrowserAnimationsModule,MatToolbarModule,MatButtonModule,MatIconModule,MatFormFieldModule,MatCardModule,MatInputModule,FlexLayoutModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
